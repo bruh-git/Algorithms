5619b74b51c14603df4f282cb517ed696fc2fc63
bruh-git <brunac.cardososilva@gmail.com>
2022-11-21 14:01:58 -0300

    lint

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,11 @@
 def is_anagram(first_string, second_string):
-  
+    first_string = first_string.lower()
+    second_string = second_string.lower()
+    first_string = first_string.replace(" ", "")
+    second_string = second_string.replace(" ", "")
+    first_string = sorted(first_string)
+    second_string = sorted(second_string)
+    if first_string == second_string:
+        return True
+    else:
+        return False
