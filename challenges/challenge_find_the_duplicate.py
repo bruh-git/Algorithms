27b32ded5230b6374f443f17d4e51441a5186dbb
bruh-git <brunac.cardososilva@gmail.com>
2022-11-21 13:57:22 -0300

    feat: req 05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,8 @@
 def find_duplicate(nums):
-    
-    raise NotImplementedError
+    if len(nums) == 0:
+        return False
+    sorted_nums = sorted(nums)
+    for i in range(len(sorted_nums) - 1):
+        if sorted_nums[i] == sorted_nums[i + 1]:
+            return True
+    return False
