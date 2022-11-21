1bd95921d77186a82c776f7e0e4edb2653c089c9
bruh-git <brunac.cardososilva@gmail.com>
2022-11-21 13:44:07 -0300

    feat: req 06 e fix: req 03

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,1 @@
 def is_anagram(first_string, second_string):
-
-    raise NotImplementedError
--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,6 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
     if len(word) == 0:
         return False
+    if len(word) == '':
+        return False
     if low_index >= high_index:
         return True
     if word[low_index] != word[high_index]:
