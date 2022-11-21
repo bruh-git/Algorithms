cda9b517c5a509ef3daca9c5522d04111165bdf4
bruh-git <brunac.cardososilva@gmail.com>
2022-11-21 11:57:47 -0300

    feat: req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,11 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    quantity = 0
     if type(target_time) != int:
         return None
-    if type(permanence_period) != int:
-        return None
-    else:
-        while target_time > permanence_period:
-            target_time -= permanence_period
-            quantity += 1
-        return quantity
+
+    quantity = 0
+    for i in permanence_period:
+        if type(i[0]) != int or type(i[1]) != int:
+            return None
+        else:
+            quantity += 1 if (i[0] <= target_time <= i[1]) else 0
+    return quantity
